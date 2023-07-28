5ca3f6bf5d1671ad4dc24d0277e036f0ae79b31a
Abdul Aziz <dev.abdaziz@outlook.com>
2023-07-28 16:17:00 +0700

    baru sampe resume tapi blm selesai

--- src/data.jsx
+++ src/data.jsx
@@ -61,25 +61,25 @@ export const personalInfo = [
   {
     id: 1,
     title: 'First Name : ',
-    description: 'Steve',
+    description: 'Abdul',
   },
 
   {
     id: 2,
     title: 'Last Name : ',
-    description: 'Milner',
+    description: 'Aziz',
   },
 
   {
     id: 3,
     title: 'Age : ',
-    description: '27 Years',
+    description: '33 Years',
   },
 
   {
     id: 4,
     title: 'Nationality : ',
-    description: 'Tunisian',
+    description: 'Indonesian',
   },
 
   {
@@ -91,31 +91,13 @@ export const personalInfo = [
   {
     id: 6,
     title: 'Address : ',
-    description: 'Tunis',
+    description: 'DKI Jakarta',
   },
 
   {
     id: 7,
-    title: 'Phone : ',
-    description: '+21621184010',
-  },
-
-  {
-    id: 8,
     title: 'Email : ',
-    description: 'you@mail.com',
-  },
-
-  {
-    id: 9,
-    title: 'Skype : ',
-    description: 'steve.milner',
-  },
-
-  {
-    id: 10,
-    title: 'Langages : ',
-    description: 'French, English',
+    description: 'dev.abdaziz@outlook.com',
   },
 ];
 
