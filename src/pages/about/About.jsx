924ab3b32c4720d153d67ef488ec9d656f497915
Abdul Aziz <dev.abdaziz@outlook.com>
2023-08-10 15:14:11 +0700

    penmabahan cv

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Info from "../../components/Info";
 import Stats from "../../components/Stats";
 import { FaDownload } from "react-icons/fa";
-import CV from "../../assets/steve-Cv.pdf";
+import CV from "../../assets/Abdul Aziz - CV.pdf";
 import Skills from "../../components/Skills";
 import { certificate, resume } from "../../data";
 import ResumeItem from "../../components/ResumeItem";
